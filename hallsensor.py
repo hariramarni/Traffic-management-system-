f15f33ee19f790f5cae6c2992e1df87c8d80bd53
hariramarni <74141533+hariramarni@users.noreply.github.com>
2020-11-08 19:26:36 +0530

    Add files via upload

---
+++ hallsensor.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as gpio
+gpio.setmode(gpio.BCM)
+gpio.setwarnings(False)
+hallpin = 2
+ledpin = 3
+gpio.setup( hallpin, gpio.IN)
+gpio.setup(ledpin, gpio.OUT)
+gpio.output(ledpin, False)
+while True:
+    if(gpio.input(hallpin) == False):
+        gpio.output(ledpin, True)
+        print("magnet detected")
+    else:
+        gpio.output(ledpin, False)
+        print("magnetic field not detected")
